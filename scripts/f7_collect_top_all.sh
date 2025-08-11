3bb96df4568fe9cefc1e61992c4013e3c5c1f733
jiaxinliu <jiaxinliu.victor@gmail.com>
2025-08-11 00:27:27 -0400

    Figure7:FixWorkflowBug

--- scripts/f7_collect_top_all.sh
+++ scripts/f7_collect_top_all.sh
@@ -7,14 +7,14 @@ GROUP="${GROUP:-10}"
 echo "==> Collecting top-of-figure logs (SIZE=$SIZE, GROUP=$GROUP)"
 
 # Solo: CPU only
-#for e in gs mw la; do
-#  ./scripts/f7_collect_mem_perf.sh "$e" solo "$SIZE" "$GROUP"
-#done
+for e in gs mw la; do
+  ./scripts/f7_collect_mem_perf.sh "$e" solo "$SIZE" "$GROUP"
+done
 
 # Congested: CPU + GPU
-#for e in gs mw la; do
-#  ./scripts/f7_collect_mem_perf.sh "$e" congested "$SIZE" "$GROUP"
-#done
+for e in gs mw la; do
+  ./scripts/f7_collect_mem_perf.sh "$e" congested "$SIZE" "$GROUP"
+done
 for e in mw3 la2; do
   ./scripts/f7_collect_mem_ncu.sh "$e" congested "$SIZE" "$GROUP"
 done
